803b798acfdb7c4ecfbe1dce9e172547e347550c
Revangga <wisnugrohorevangga@gmail.com>
2018-05-08 10:56:32 +0700

    updated
    1.1

--- js/script.js
+++ js/script.js
@@ -10,3 +10,38 @@ $('.page-scroll').on('click', function(e) {
 
  e.preventDefault();
 });﻿
+
+// paralax
+
+$(window).on('load', function() {
+  $('.pKiri').addClass('pMuncul');
+  $('.pKanan').addClass('pMuncul');
+})
+
+
+$(window).scroll(function() {
+  var wScroll = $(this).scrollTop();
+
+  $('.jumbotron img').css({
+    'transform' : 'translate(0px, '+ wScroll/3 + '%)'
+  })
+
+  $('.jumbotron h1').css({
+    'transform' : 'translate(0px, '+ wScroll/1.2 + '%)'
+  })
+
+  $('.jumbotron p').css({
+    'transform' : 'translate(0px, '+ wScroll/.6 + '%)'
+  })
+
+  //portofolio
+if(wScroll > $('.portofolio').offset().top - 250) {
+    $('.portofolio .thumbnail').each(function(i){
+      setTimeout(function() {
+        $('.portofolio .thumbnail').eq(i).addClass('muncul');
+      }, 200 * i);
+    });
+}
+
+
+})
